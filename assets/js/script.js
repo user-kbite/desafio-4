9c78be7672ddf39b7b59f0416f916276f4e6addd
user-kbite <69611562+user-kbite@users.noreply.github.com>
2021-07-25 23:15:21 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 const lock = document.querySelector(".lock");
 
 lock.addEventListener("click", function() {
-	/*if (lock.src === "./assets/image/lock.png") {
+	if (lock.src === "./assets/image/lock.png") {
 		lock.src = "./assets/image/lock.png"
-		console.log(lock.src)
 	} else {
 		lock.src = "./assets/image/unlock.png"
-		console.log(lock.src)
-	}*/
+	}
 	alert(lock.src)
 })
