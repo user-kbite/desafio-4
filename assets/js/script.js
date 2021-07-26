eec877fecfd49beed43ef568425d2cc8dc9d9a53
user-kbite <69611562+user-kbite@users.noreply.github.com>
2021-07-25 23:13:13 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const lock = document.querySelector(".lock");
+
+lock.addEventListener("click", function() {
+	/*if (lock.src === "./assets/image/lock.png") {
+		lock.src = "./assets/image/lock.png"
+		console.log(lock.src)
+	} else {
+		lock.src = "./assets/image/unlock.png"
+		console.log(lock.src)
+	}*/
+	alert(lock.src)
+})
